61ba6052cde418534ea31f37b5a8d71ae28d0e7d
Fernando Duarte <fdduarte@uc.cl>
2020-06-15 10:29:34 -0400

    ft: parse output error handling

--- SSTPA/modules/model_stats.py
+++ SSTPA/modules/model_stats.py
@@ -52,23 +52,27 @@ class ModelStats:
   @staticmethod
   def parse_gurobi_output(model_vars, matches):
     file_lines = dict()
-    for var in model_vars:
-      if "x" in str(var):
-        _, var, _, value = str(var).split()
-        match, date = var.split(",")
-        value = int(float(value.strip(")>")))
-        match = int(match.strip("x["))
-        date = int(date.strip("]"))
-        if date not in file_lines.keys():
-          file_lines[date] = list()
-        if value:
-          file_lines[date].append(f",{matches[match]['home']},{matches[match]['away']}\n")
-    with open("output/programacion.csv", "w", encoding="UTF-8") as infile:
-      infile.write("jornada, local, visita\n")
-      for date in file_lines.keys():
-        infile.write(f"{date},,\n")
-        for line in file_lines[date]:
-          infile.write(line)
+    try:
+      for var in model_vars:
+        if "x" in str(var):
+          _, var, _, value = str(var).split()
+          match, date = var.split(",")
+          value = int(float(value.strip(")>")))
+          match = int(match.strip("x["))
+          date = int(date.strip("]"))
+          if date not in file_lines.keys():
+            file_lines[date] = list()
+          if value:
+            file_lines[date].append(f",{matches[match]['home']},{matches[match]['away']}\n")
+      with open("output/programacion.csv", "w", encoding="UTF-8") as infile:
+        infile.write("jornada, local, visita\n")
+        for date in file_lines.keys():
+          infile.write(f"{date},,\n")
+          for line in file_lines[date]:
+            infile.write(line)
+    except ValueError:
+      with open("output/programacion.csv", "w", encoding="UTF-8") as infile:
+        infile.write("Modelo Infactible")
 
   def parse_logs(self, logs):
     self.logs = logs
